7f560a35aadf0e841542fd701a1b1ca51121ee6e
Vinu567 <routhuvinod567@gmail.c>
2022-11-12 16:22:23 +0530

    ggg

---
+++ 8.String/covlowtup.c
@@ -0,0 +1,15 @@
+//wacp to convert lowercase alphabets into uppercase alphabets  in a string given by the user.
+
+#include<stdio.h>
+void main()
+{
+	int size;
+	printf("enter a size of a string...");
+	scanf("%d",&size);
+	char a[size];
+	printf("enter a string in agiven range");
+	gets(a);
+	puts(a);
+}
+
+
